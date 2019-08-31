b86da84565b73618d9eb2ad31e40b325d60eb54c
cronugs <cronugs@outlook.com>
2019-08-31 09:45:39 +0200

    added a registration form, template and route with wtforms

--- myBlog.py
+++ myBlog.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template, url_for
+from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '1f67b3678156205853ddc3ef59abafed'
 
 posts = [
     {
@@ -27,6 +30,18 @@ def home():
 @app.route("/about")
 def about():
     return render_template('about.html')
+
+
+@app.route("/register")
+def register():
+    form = RegistrationForm()
+    return render_template('register.html', form=form)
+
+
+@app.route("/login")
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Login', form=form)
 
 
 if __name__ == '__main__':
